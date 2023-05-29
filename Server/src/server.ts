5005fe2b7f2ef6375f45814eb211c6ac3145869f
campbell-mason <campbell.mason@hotmail.co.nz>
2023-05-30 09:32:52 +1200

    set up server

---
+++ Server/src/server.ts
@@ -0,0 +1,10 @@
+import env from "dotenv";
+import app from "./app";
+
+env.config()
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`)
+})
+
