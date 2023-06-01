aaf25c28510e1d0f7f0092948a629cfefc7b4462
campbell-mason <campbell.mason@hotmail.co.nz>
2023-06-02 08:57:25 +1200

    setup database connectio

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,8 +1,9 @@
 import env from "dotenv";
 import app from "./app";
+import connectDb from "./db";
 
 env.config()
-
+connectDb()
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
