6391585f3e341efeeb35e00100d4e33e87cb9dc9
JonoPadoa <jonathanp@missionreadyhq.com>
2023-06-05 13:09:58 +1200

    half way through adding the code for property search page.  Additional component of slider added and will have to refactor that later.

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from './pages/Home'
 import PropertySearchPage from "./pages/PropertySearchPage";
 
 function App() {
   return (
     <>
     <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Property" element={<PropertySearchPage />} />
+         <Route path="/Property" element={<PropertySearchPage />} />
     </Routes>
     </>
   );
