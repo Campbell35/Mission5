c78ad3c1fadbf7932bbf257a9ce2c3db0049ec01
campbell-mason <campbell.mason@hotmail.co.nz>
2023-06-07 08:12:29 +1200

    made changes

--- Client/src/pages/PropertySearchPage.tsx
+++ Client/src/pages/PropertySearchPage.tsx
@@ -1,57 +1,69 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
 import style from "./PropertySearchPage.module.css";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
 
+
+interface Property {
+  id: number;
+  Address: string;
+  Suburb: string;
+  Price: number;
+  Bedrooms: number;
+}
 const MIN = 100;
 const MAX = 5000;
-// interface Property {
-//   id: number;
-//   title: string;
-//   price: number;
-//   location: string;
-// }
 
-// const properties: Property[] = [];
-
-// fetch('http://localhost:3000/api/properties')
-//   .then(response => response.json())
-//   .then(data => {
-//     // Process the retrieved data
-//     properties.push(...data);
-//     console.log('Retrieved properties:', properties);
-//   })
-//   .catch(error => {
-//     // Handle any errors
-//     console.error('Error retrieving data:', error);
-//   });
-
-// const PropertySearchPage: React.FC = () => {
-//   const [searchTerm, setSearchTerm] = useState('');
-//   const [searchResults, setSearchResults] = useState<Property[]>([]);
-
-//   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     setSearchTerm(event.target.value);
-//   };
-
-//   const handleSearchSubmit = (event: React.FormEvent) => {
-//     event.preventDefault();
-
-//     const results = properties.filter(property =>
-//       property.title.toLowerCase().includes(searchTerm.toLowerCase())
-//     );
-//     setSearchResults(results);
-//   };
-
-function PropertySearchPage() {
+const PropertySearchPage: React.FC = () => {
   const [values, setValues] = useState<[number, number]>([MIN, MAX]);
-  console.log("values:", values);
+  const [address, setAddress] = useState('');
+  const [suburb, setSuburb] = useState('');
+  const [searchResults, setSearchResults] = useState<Property[]>([]);
+  const properties: Property[] = [];
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/properties')
+      .then(response => response.json())
+      .then(data => {
+        properties.push(...data);
+        console.log('Retrieved properties:', properties);
+      })
+      .catch(error => {
+        console.error('Error retrieving data:', error);
+      });
+  }, []);
+
+   const handleAddressChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newAddress = event.target.value;
+    setAddress(newAddress);
+  };
+
+  const handleSuburbChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newSuburb = event.target.value;
+    setSuburb(newSuburb);
+  };
+  
+
+   const handleSearchSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    // Call the filter function to update searchResults on form submission
+    const filteredResults = filterProperties(properties, address, suburb);
+    setSearchResults(filteredResults);
+  };
+
+   const filterProperties = (properties: Property[], address: string, suburb: string): Property[] => {
+    return properties.filter((property) =>
+      property.Address.toLowerCase().includes(address.toLowerCase()) &&
+      property.Suburb.toLowerCase().includes(suburb.toLowerCase())
+    );
+  };
 
   const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = parseInt(event.target.value);
     setValues([MIN, newValue]);
-  
-  }
+    // Handle slider change
+  };
 
   return (
     <div className={style.TheMain_page}>
@@ -78,219 +90,227 @@ function PropertySearchPage() {
         </div>
       </div>
     
-    <div className={style.mainDiv}>
-      <div className={style.card_main_container}>
-        <div className={style.container_left}>
-          <div className={style.card_inputDiv}>
-            <div className={style.card_input_select_region}>
-              <span>Select region...</span>
-              <br />
-              <input
-                type="text"
-                placeholder="EG: Auckland"
-                // value={inputValue}
-                // onChange={handleInput}
+      <div className={style.mainDiv}>
+        <div className={style.card_main_container}>
+          <div className={style.container_left}>
+            <div className={style.card_inputDiv}>
+              <div className={style.card_input_select_region}>
+                <span>Select region...</span>
+                <br />
+                <input
+                  type="text"
+                  placeholder="EG: Auckland"
+                  value={address}
+                  onChange={handleAddressChange}
+                />
+              </div>
+    
+              <div className={style.card_input_select_suburb}>
+                <span>Select suburb...</span>
+                <br />
+                <input
+                  type="text"
+                  placeholder="EG: Browns Bay"
+                  value={suburb}
+                  onChange={handleSuburbChange}
+                />
+              </div>
+            </div>
+    
+            <div className={style.button_container}>
+              <button className={style.search_buttons}>Price</button>
+              <button className={style.search_buttons}>Bed</button>
+              <button className={style.search_buttons}>Bath</button>
+              <button className={style.search_buttons}>Pets Allowed</button>
+            </div>
+          </div>
+          <div className={style.container_right}>
+            <div className={style.button_right_cluster}>
+              <button className={style.search_buttons}>Search Map</button>
+              <button className={style.search_buttons}>Popular</button>
+              <button className={style.search_buttons}>Save Search</button>
+              <button className={style.search_buttons}>Random</button>
+            </div>
+          </div>
+        </div>
+    
+        <div className={style.popUpmainDiv}>
+          <div className={style.popUpcontainer}>
+            <div className={style.popUpcontainer_left}>
+              <div className={style.popUpsliderDiv}>
+                <div className={style.slider_div_main}>
+                  <div className={style.SLIDERmainDiv}>
+                    <div className={style.SLIDERsliderMain}>
+                      <h3>
+                        Price <span>Range</span>
+                      </h3>
+                      <div className={style.SLIDERslider_value}>$100.00 - $5000.00</div>
+                      <small>Current Range: ${values[1]}.00</small>
+    
+                      <input
+                        type="range"
+                        className={style.SLIDERtheslider}
+                        onChange={handleSliderChange}
+                        value={values[1]}
+                        min={MIN}
+                        max={MAX}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+              <div className={style.popUpsliderDiv_buttons}>
+                <button className={style.popUpsliderDiv_button_ONE}>CLEAR</button>
+                <button className={style.popUpsliderDiv_button_TWO}>APPLY</button>
+              </div>
+            </div>
+    
+            <div className={style.popUpcontainer_right}>
+              <img
+                className={style.popUpfeaturedImage}
+                src="./images/featured-apartment.jpg"
+                alt="featured apartment"
+                width="220"
+                height="188"
+              />
+              <span className={style.popUpfeaturedLocation}>
+                <span>Featured property in:</span>
+                <br></br>
+                <br></br>
+                <span className={style.popUpfeaturedLocation_city}>Auckland</span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+      <div className={style.CARDmainDiv}>
+        <div className={style.CARDcontainer}>
+          <div className={style.CARDcardDiv}>
+            <div>
+              <img
+                className={style.CARDpropertyImage}
+                src="./images/Property1.jpg"
+                alt="property"
+                width="300"
+                height="200"
               />
             </div>
-
-            <div className={style.card_input_select_suburb}>
-              <span>Select suburb...</span>
+            <div className={style.CARDpropertyDetails}>
+              <span className={style.CARDpropertyAddress}>
+                21/105 Kingston Street
+              </span>
               <br />
-              <input
-                type="text"
-                placeholder="EG: Browns Bay"
-                // value={inputValue}
-                // onChange={handleInput}
+              <span className={style.CARDpropertyCity}>Auckland City</span>
+              <br />
+              <span className={style.CARDpropertyPrice}>$1050.00 pw</span>
+            </div>
+          </div>
+          <div className={style.CARDcardDiv}>
+            <div>
+              <img
+                className={style.CARDpropertyImage}
+                src="./images/Property2.jpg"
+                alt="property"
+                width="300"
+                height="200"
               />
             </div>
-          </div>
-
-          <div className={style.button_container}>
-            <button className={style.search_buttons}>Price</button>
-            <button className={style.search_buttons}>Bed</button>
-            <button className={style.search_buttons}>Bath</button>
-            <button className={style.search_buttons}>Pets Allowed</button>
-          </div>
-        </div>
-        <div className={style.container_right}>
-          <div className={style.button_right_cluster}>
-            <button className={style.search_buttons}>Search Map</button>
-            <button className={style.search_buttons}>Popular</button>
-            <button className={style.search_buttons}>Save Search</button>
-            <button className={style.search_buttons}>Random</button>
-          </div>
-        </div>
-      </div>
-
-      <div className={style.popUpmainDiv}>
-      <div className={style.popUpcontainer}>
-        <div className={style.popUpcontainer_left}>
-          <div className={style.popUpsliderDiv}>
-            <div className={style.slider_div_main}>
-           {/* SLIDERD DIV */}
-           <div className={style.SLIDERmainDiv}>
-      <div className={style.SLIDERsliderMain}>
-        <h3>
-          Price <span>Range</span>
-        </h3>
-        <div className={style.SLIDERslider_value}>$100.00 - $5000.00</div>
-        <small>Current Range: ${values[1]}.00</small>
-
-        <input
-          type="range"
-          className={style.SLIDERtheslider}
-          onChange={handleSliderChange}
-          value={values[1]}
-          min={MIN}
-          max={MAX}
-        />
-      </div>
-    </div>
+            <div className={style.CARDpropertyDetails}>
+              <span className={style.CARDpropertyAddress}>5/12 Queens Rise</span>
+              <br />
+              <span className={style.CARDpropertyCity}>Auckland City</span>
+              <br />
+              <span className={style.CARDpropertyPrice}>$850.00 pw</span>
             </div>
-            
           </div>
-          <div className={style.popUpsliderDiv_buttons}>
-            <button className={style.popUpsliderDiv_button_ONE}>CLEAR</button>
-            <button className={style.popUpsliderDiv_button_TWO}>APPLY</button>
-          </div>
-        </div>
-        <div className={style.popUpcontainer_right}>
-          <img
-            className={style.popUpfeaturedImage}
-            src="./images/featured-apartment.jpg"
-            alt="featured apartment"
-            width="220"
-            height="188"
-          />
-          <span className={style.popUpfeaturedLocation}>
-            <span>Featured property in:</span>
-            <br></br>
-            <br></br>
-            <span className={style.popUpfeaturedLocation_city}>Auckland</span>
-          </span>
-        </div>
-      </div>
-    </div>
-    </div>
-    {/* PROPERTY CARDS SECTION BELOW*/}
-    <div className={style.CARDmainDiv}>
-      <div className={style.CARDcontainer}>
-        <div className={style.CARDcardDiv}>
-          <div>
-            <img
-              className={style.CARDpropertyImage}
-              src="./images/Property1.jpg"
-              alt="property"
-              width="300"
-              height="200"
-            />
-          </div>
-          <div className={style.CARDpropertyDetails}>
-            <span className={style.CARDpropertyAddress}>
-              21/105 Kingston Street
-            </span>
-            <br />
-            <span className={style.CARDpropertyCity}>Auckland City</span>
-            <br />
-            <span className={style.CARDpropertyPrice}>$1050.00 pw</span>
-          </div>
-        </div>
-        <div className={style.CARDcardDiv}>
-          <div>
-            <img
-              className={style.CARDpropertyImage}
-              src="./images/Property2.jpg"
-              alt="property"
-              width="300"
-              height="200"
-            />
-          </div>
-          <div className={style.CARDpropertyDetails}>
-            <span className={style.CARDpropertyAddress}>5/12 Queens Rise</span>
-            <br />
-            <span className={style.CARDpropertyCity}>Auckland City</span>
-            <br />
-            <span className={style.CARDpropertyPrice}>$850.00 pw</span>
-          </div>
-        </div>
-        <div className={style.CARDcardDiv}>
-          <div>
-            <img
-              className={style.CARDpropertyImage}
-              src="./images/Property3.jpg"
-              alt="property"
-              width="300"
-              height="200"
-            />
-          </div>
-          <div className={style.CARDpropertyDetails}>
-            <span className={style.CARDpropertyAddress}>17/18 Apt B - Koff St</span>
-            <br />
-            <span className={style.CARDpropertyCity}>Mission Bay</span>
-            <br />
-            <span className={style.CARDpropertyPrice}>$1350.00 pw</span>
-          </div>
-        </div>
-        <div className={style.CARDcardDiv}>
-          <div>
-            <img
-              className={style.CARDpropertyImage}
-              src="./images/Property4.jpg"
-              alt="property"
-              width="300"
-              height="200"
-            />
-          </div>
-          <div className={style.CARDpropertyDetails}>
-            <span className={style.CARDpropertyAddress}>
-              4/17 The Spencer on Byron
-            </span>
-            <br />
-            <span className={style.CARDpropertyCity}>Takapuna</span>
-            <br />
-            <span className={style.CARDpropertyPrice}>$985.00 pw</span>
+          <div className={style.CARDcardDiv}>
+            <div>
+              <img
+                className={style.CARDpropertyImage}
+                src="./images/Property3.jpg"
+                alt="property"
+                width="300"
+                height="200"
+              />
+            </div>
+            <div className={style.CARDpropertyDetails}>
+              <span className={style.CARDpropertyAddress}>17/18 Apt B - Koff St</span>
+              <br />
+              <span className={style.CARDpropertyCity}>Auckland City</span>
+              <br />
+              <span className={style.CARDpropertyPrice}>$620.00 pw</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    {/* PROPERTY CARDS SECTION ABOVE*/}
+    
+      <div className={style.CARDmainDiv}>
+        <div className={style.CARDcontainer}>
+          <div className={style.CARDcardDiv}>
+            <div>
+              <img
+                className={style.CARDpropertyImage}
+                src="./images/Property4.jpg"
+                alt="property"
+                width="300"
+                height="200"
+              />
+            </div>
+            <div className={style.CARDpropertyDetails}>
+              <span className={style.CARDpropertyAddress}>4/25 Princes Street</span>
+              <br />
+              <span className={style.CARDpropertyCity}>Auckland City</span>
+              <br />
+              <span className={style.CARDpropertyPrice}>$700.00 pw</span>
+            </div>
+          </div>
+          <div className={style.CARDcardDiv}>
+            <div>
+              <img
+                className={style.CARDpropertyImage}
+                src="./images/Property5.jpg"
+                alt="property"
+                width="300"
+                height="200"
+              />
+            </div>
+            <div className={style.CARDpropertyDetails}>
+              <span className={style.CARDpropertyAddress}>5A Faraday Street</span>
+              <br />
+              <span className={style.CARDpropertyCity}>Auckland City</span>
+              <br />
+              <span className={style.CARDpropertyPrice}>$725.00 pw</span>
+            </div>
+          </div>
+          <div className={style.CARDcardDiv}>
+            <div>
+              <img
+                className={style.CARDpropertyImage}
+                src="./images/Property6.jpg"
+                alt="property"
+                width="300"
+                height="200"
+              />
+            </div>
+            <div className={style.CARDpropertyDetails}>
+              <span className={style.CARDpropertyAddress}>10 Queen Street</span>
+              <br />
+              <span className={style.CARDpropertyCity}>Auckland City</span>
+              <br />
+              <span className={style.CARDpropertyPrice}>$520.00 pw</span>
+            </div>
+          </div>
+        </div>
+      </div>
       <div>
         <Footer />
       </div>
     </div>
   );
-}
+};
 
 export default PropertySearchPage;
 
-//     <div>
-//       <h1>Property Search</h1>
 
-//       <form onSubmit={handleSearchSubmit}>
-//         <input
-//           type="text"
-//           placeholder="Search by property title"
-//           value={searchTerm}
-//           onChange={handleSearchChange}
-//         />
-//         <button type="submit">Search</button>
-//       </form>
-
-//       <h2>Search Results</h2>
-
-//       {searchResults.length > 0 ? (
-//         <ul>
-//           {searchResults.map(property => (
-//             <li key={property.id}>
-//               <h3>{property.title}</h3>
-//               <p>Price: ${property.price}</p>
-//               <p>Location: {property.location}</p>
-//             </li>
-//           ))}
-//         </ul>
-//       ) : (
-//         <p>No results found.</p>
-//       )}
-//     </div>
-//   );
-// };
--- Server/src/models/PropertyModel.ts
+++ Server/src/models/PropertyModel.ts
@@ -1,15 +1,34 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
+// Define the interface for the Property document
 interface IProperty extends Document {
-  name: string;
-  description: string;
+  id: number;
+  Address: string;
+  Suburb: string;
+  Price: number;
+  Bedrooms: number;
+  // Add more properties as needed
 }
 
+// Define the schema for the Property document
 const propertySchema = new Schema<IProperty>({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
+  id: { type: Number, required: true },
+  Address: { type: String, required: true },
+  Suburb: {type: String, required: true},
+  Price: {type: Number, required: true},
+  Bedrooms: {type: Number, required: true}
 });
 
+// Connect to the "MetroProperties" database in MongoDB Atlas
+mongoose.connect('mongodb+srv://campbellm:FqVCwukZyT6vIwNZ@Mission-5-cluster.mongodb.net/MetroProperties?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  });
+
+// Create the Property model
 const PropertyModel = mongoose.model<IProperty>('Property', propertySchema);
 
 export default PropertyModel;
